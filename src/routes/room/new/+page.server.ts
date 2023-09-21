e3c1d692ecd4495e7f3c2bd7b634113da0566298
Alex Way <alexwa@softcat.com>
2023-09-21 12:54:53 +0100

    Fix issue with redirect

--- src/routes/room/new/+page.server.ts
+++ src/routes/room/new/+page.server.ts
@@ -1,8 +1,8 @@
 import { redirect } from "@sveltejs/kit";
-import type { RequestHandler } from "./$types";
 import { randomUUID } from "crypto";
+import type { PageLoad } from "./$types";
 
-export const GET: RequestHandler = () => {
+export const load: PageLoad = ({ params }) => {
 	const random = randomUUID();
 
 	throw redirect(303, `/room/${random}`);
