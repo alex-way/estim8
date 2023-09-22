27c6b9d17caaa12ec4e6545fdef849197cec5ef8
Alex Way <alexwa@softcat.com>
2023-09-22 20:07:18 +0100

    Add tooltips for features

--- src/routes/room/[id=uuid]/components/RoomConfig.svelte
+++ src/routes/room/[id=uuid]/components/RoomConfig.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import type { RoomState } from '$lib/roomState';
 	import Badge from '$lib/components/ui/badge/badge.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import type { RoomState } from '$lib/roomState';
+	import * as Tooltip from '$lib/components/ui/tooltip';
 	import Context from './Context.svelte';
 
 	export let roomState: RoomState;
@@ -12,16 +13,32 @@
 </script>
 
 <div class="border-white border-opacity-20 border-l-2 h-full p-4">
-	<form method="post" action="?/inverseParticipation" use:enhance class="inline-block my-4">
-		<input type="hidden" name="deviceId" value={deviceId} />
-		<Button type="submit" size="sm" class="inline-block">{participating ? 'Participating' : 'Observing'}</Button>
-	</form>
+	<div class="flex gap-2 my-2">
+		<form method="post" action="?/inverseParticipation" use:enhance>
+			<input type="hidden" name="deviceId" value={deviceId} />
+			<Tooltip.Root>
+				<Tooltip.Trigger>
+					<Button type="submit" size="sm" class="inline-block">{participating ? 'Participating' : 'Observing'}</Button>
+				</Tooltip.Trigger>
+				<Tooltip.Content>
+					<p>Changing this option allows you to sit out of the voting and observe the results.</p>
+				</Tooltip.Content>
+			</Tooltip.Root>
+		</form>
 
-	<form method="post" action="?/inverseSnooping" use:enhance class="inline-block my-4">
-		<Button type="submit" size="sm" class="inline-block"
-			>{roomState.config.allowObserversToSnoop ? 'Disable Snooping' : 'Allow Snooping'}</Button
-		>
-	</form>
+		<form method="post" action="?/inverseSnooping" use:enhance>
+			<Tooltip.Root>
+				<Tooltip.Trigger>
+					<Button type="submit" size="sm" class="inline-block"
+						>{roomState.config.allowObserversToSnoop ? 'Disable Snooping' : 'Allow Snooping'}</Button
+					>
+				</Tooltip.Trigger>
+				<Tooltip.Content>
+					<p>Snooping allows observers to view results before they've been revealed.</p>
+				</Tooltip.Content>
+			</Tooltip.Root>
+		</form>
+	</div>
 
 	<h1 class="text-xl">Participants</h1>
 
