c92f27a69694350c8e1e0e80963427e028771c89
Alex Way <way.alexander@yahoo.co.uk>
2024-04-16 20:13:07 +0100

    refactor: Remove redundant baseRoom

--- src/lib/roomState/index.ts
+++ src/lib/roomState/index.ts
@@ -2,14 +2,133 @@ import { randomUUID } from "node:crypto";
 import { db } from "$db";
 import schema from "$db/schema";
 import { pusher } from "$hooks/server";
-import type { Choice, RoomState } from "$lib/types";
+import type { CardBack, Choice, RoomState, RoomUser } from "$lib/types";
 import { eq, sql } from "drizzle-orm";
 import { DEFAULT_CHOICES, getChannelName } from "../constants";
-import { BaseRoom } from "./base";
 
-export class Room extends BaseRoom {
+export class Room {
 	id: string;
 	state: RoomState;
+	#lastSavedState: string;
+
+	setAdmin(deviceId: string) {
+		this.state.adminDeviceId = deviceId;
+	}
+
+	setNameForDeviceId(deviceId: string, name: string) {
+		const user = this.getUserOrDefault(deviceId);
+		user.name = name;
+	}
+
+	getDeviceIdFromName(name: string): string | null {
+		for (const [deviceId, user] of Object.entries(this.state.users)) {
+			if (user.name === name) {
+				return deviceId;
+			}
+		}
+		return null;
+	}
+
+	getUserFromName(name: string): RoomUser | undefined {
+		for (const [_, user] of Object.entries(this.state.users)) {
+			if (user.name === name) {
+				return user;
+			}
+		}
+	}
+
+	getUserFromDeviceId(deviceId: string): RoomUser | undefined {
+		return this.state.users[deviceId];
+	}
+
+	setChosenNumberForDeviceId(deviceId: string, choice: Choice) {
+		const user = this.getUserOrDefault(deviceId);
+		user.choice = choice;
+	}
+
+	invertShowResults() {
+		this.state.showResults = !this.state.showResults;
+	}
+
+	invertSnooping() {
+		this.state.config.allowObserversToSnoop = !this.state.config.allowObserversToSnoop;
+	}
+
+	invertAllowUnknown() {
+		this.state.config.allowUnknown = !this.state.config.allowUnknown;
+		if (!this.state.config.allowUnknown) {
+			for (const [_, user] of Object.entries(this.state.users).filter(([_, user]) => user.choice === "?")) {
+				user.choice = null;
+			}
+		}
+	}
+
+	clearSelectedNumbers() {
+		for (const [_, user] of Object.entries(this.state.users)) {
+			user.choice = null;
+		}
+		this.state.showResults = false;
+	}
+
+	updateSelectableNumbers(selectableNumbers: number[]) {
+		this.state.config.selectableNumbers = selectableNumbers.sort((a, b) => a - b);
+		this.clearSelectedNumbers();
+	}
+
+	setCardBackForDeviceId(deviceId: string, cardBack: CardBack) {
+		const user = this.getUserOrDefault(deviceId);
+		user.config = { ...user.config, cardBack };
+	}
+
+	getUserOrDefault(deviceId: string): RoomUser {
+		const existingUser = this.getUserFromDeviceId(deviceId);
+		if (existingUser) return existingUser;
+
+		this.state.users[deviceId] = {
+			deviceId,
+			name: "",
+			choice: null,
+			isParticipant: true,
+		};
+
+		return this.state.users[deviceId];
+	}
+
+	setUserAsObserver(deviceId: string) {
+		const user = this.getUserOrDefault(deviceId);
+		user.isParticipant = false;
+	}
+
+	setUserAsParticipant(deviceId: string) {
+		const user = this.getUserOrDefault(deviceId);
+		user.isParticipant = true;
+	}
+
+	inverseUserParticipation(deviceId: string) {
+		const user = this.getUserOrDefault(deviceId);
+		user.isParticipant = !user.isParticipant;
+	}
+
+	setUserParticipation(deviceId: string, isParticipant: boolean) {
+		const user = this.getUserOrDefault(deviceId);
+		user.isParticipant = isParticipant;
+	}
+
+	removeUsersNotInRoom(usersInRoom: string[]) {
+		const usersNotInRoom = Object.keys(this.state.users).filter((user) => !usersInRoom.includes(user));
+
+		for (const user of usersNotInRoom) {
+			this.removeUser(user);
+		}
+	}
+
+	removeUser(deviceId: string) {
+		delete this.state.users[deviceId];
+	}
+
+	isModified(): boolean {
+		return this.#lastSavedState !== JSON.stringify(this.state);
+	}
 
 	/**
 	 * NOTE: The constructor is now `private`.
@@ -27,9 +146,9 @@ export class Room extends BaseRoom {
 	 * @private
 	 */
 	private constructor(id: string, state: RoomState) {
-		super(state);
 		this.id = id;
 		this.state = state;
+		this.#lastSavedState = JSON.stringify(state);
 	}
 
 	static async createRoom(config: { id?: string; choices?: number[] }, adminDeviceId?: string) {
